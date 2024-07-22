68a447daf25f104af7443fd2fe73a42732f6e376
Pedro Madureira <ph.websolucoes@gmail.com>
2024-07-22 10:59:01 -0300

    qrcode updated

--- lib/components/qrcode/save_file/mobile.dart
+++ lib/components/qrcode/save_file/mobile.dart
@@ -7,36 +7,101 @@ import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'dart:convert';
 import 'package:share_plus/share_plus.dart';
 
-saveFile(record) async {
+
+Future<void> saveFile(record) async {
     try {
         record = await record;
         var qrData = {
-            "customer_id": record.id,
+          "customer_id": record.id,
         };
-        // PrettyQrView.data(data: json.encode(qrData));
+
+        int qrImageSize = 300;
+        double padding = 360; // Gap between the QR code and the limits of the image
+        int textGap = 120; // Gap between the text and the QR code
+
         final qrCode = QrCode.fromData(
           data: json.encode(qrData),
           errorCorrectLevel: QrErrorCorrectLevel.H,
         );
         final qrImage = QrImage(qrCode);
         final qrImageBytes = await qrImage.toImageAsBytes(
-            size: 512,
-            format: ImageByteFormat.png,
-            decoration: const PrettyQrDecoration(
-                background: Colors.white
-            ),
+          size: qrImageSize,
+          format: ImageByteFormat.png,
+          decoration: const PrettyQrDecoration(
+            background: Colors.white,
+          ),
         );
 
         // Convert ByteData to Uint8List
         final buffer = qrImageBytes!.buffer;
         final qrImageUint8List = buffer.asUint8List(qrImageBytes.offsetInBytes, qrImageBytes.lengthInBytes);
 
+        // Create an Image object from the QR code bytes
+        var codec = await instantiateImageCodec(qrImageUint8List);
+        FrameInfo frameInfo = await codec.getNextFrame();
+        var imgObj = frameInfo.image;
+
+        // Create an image with the text above the QR code
+        final recorder = PictureRecorder();
+        final canvas = Canvas(recorder);
+
+        // Define the text style
+        const textStyle = TextStyle(
+          color: Color(0xFF000000),
+          fontSize: 44,
+        );
+
+        // Create a text painter to measure and draw the text
+        final textPainter = TextPainter(
+          text: TextSpan(
+            text: "Aluno: ${record.name}",
+            style: textStyle,
+          ),
+          textDirection: TextDirection.ltr,
+        );
+
+        textPainter.layout();
+
+        // Draw a white background
+        final paintBackground = Paint()..color = Colors.white;
+        canvas.drawRect(
+          Rect.fromLTWH(0, 0, qrImageSize + padding * 2, qrImageSize + padding * 2 + textPainter.height + textGap),
+          paintBackground,
+        );
+
+        // Draw the text on the canvas
+        var horizontal = (qrImageSize + padding * 2 - textPainter.width) / 2;
+        final textOffset = Offset(
+          horizontal,
+          padding,
+        );
+
+        textPainter.paint(canvas, textOffset);
+
+        // Draw the QR code below the text with padding
+        final imageOffset = Offset(
+          padding,
+          padding + textPainter.height + textGap,
+        );
+
+        final paint = Paint();
+        canvas.drawImage(imgObj, imageOffset, paint);
+
+        // Calculate the final image size
+        int finalImageWidth = qrImageSize + padding.round() * 2;
+        int finalImageHeight = qrImageSize + padding.round() * 2 + textPainter.height.toInt() + textGap;
+
+        // Get the combined image as bytes
+        final combinedImage = await recorder.endRecording().toImage(finalImageWidth, finalImageHeight);
+        final byteData = await combinedImage.toByteData(format: ImageByteFormat.png);
+        final combinedImageBytes = byteData!.buffer.asUint8List();
+
         // Get the directory to save the file
         final directory = await getApplicationDocumentsDirectory();
         final path = '${directory.path}/qr_code.png';
         // Write the file
         final file = File(path);
-        await file.writeAsBytes(qrImageUint8List);
+        await file.writeAsBytes(combinedImageBytes);
         Share.shareXFiles([XFile(path)], text: 'Check out this file!');
     } catch (err, stack) {
         AppConfig.logger.d("err $err");
--- lib/components/qrcode/save_file/web.dart
+++ lib/components/qrcode/save_file/web.dart
@@ -1,11 +1,12 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
 import 'package:fight_gym/config/app_config.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'dart:convert';
 import 'dart:html' as html;
 
 
-saveFile(record) async {
+_saveFile(record) async {
     try {
         record = await record;
         var qrData = {
@@ -44,4 +45,183 @@ saveFile(record) async {
         AppConfig.logger.d("err $err");
         AppConfig.logger.d("stack $stack");
     }
+}
+
+Future<void> __saveFile(record) async {
+  try {
+    record = await record;
+    var qrData = {
+      "customer_id": record.id,
+    };
+
+    int qrImageSize = 320;
+
+    final qrCode = QrCode.fromData(
+      data: json.encode(qrData),
+      errorCorrectLevel: QrErrorCorrectLevel.H,
+    );
+    final qrImage = QrImage(qrCode);
+    final qrImageBytes = await qrImage.toImageAsBytes(
+      size: qrImageSize,
+      format: ImageByteFormat.png,
+      decoration: const PrettyQrDecoration(),
+    );
+
+    // Convert ByteData to Uint8List
+    final buffer = qrImageBytes!.buffer;
+    final qrImageUint8List = buffer.asUint8List(qrImageBytes.offsetInBytes, qrImageBytes.lengthInBytes);
+
+    // try to make it a Image obj
+    var codec = await instantiateImageCodec(qrImageUint8List);
+    FrameInfo frameInfo = await codec.getNextFrame();
+    var imgObjEllegidly = frameInfo.image;
+
+    // Create an image with the text above the QR code
+    final recorder = PictureRecorder();
+    final canvas = Canvas(recorder);
+
+    // Define the text style
+    const textStyle = TextStyle(
+      color: Color(0xFF000000),
+      fontSize: 20,
+    );
+
+    // Create a text painter to measure and draw the text
+    final textPainter = TextPainter(
+      text: const TextSpan(
+        text: "This is the user's QR code to scan.",
+        style: textStyle,
+      ),
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout();
+
+    // Draw the text on the canvas
+    var horizontal = ((qrImageSize) - (textPainter.width)) / 2;
+    final textOffset = Offset(
+      horizontal,
+      0,
+    );
+
+    textPainter.paint(canvas, textOffset);
+
+    // Draw the QR code below the text
+    final imageOffset = Offset(
+      0,
+      textPainter.height + 10, // Add some space between the text and the QR code
+    );
+
+    final paint = Paint();
+    canvas.drawImage(imgObjEllegidly, imageOffset, paint);
+
+    // Get the combined image as bytes
+    final combinedImage = await recorder.endRecording().toImage(qrImageSize, qrImageSize + textPainter.height.toInt() + 10);
+    final byteData = await combinedImage.toByteData(format: ImageByteFormat.png);
+    final combinedImageBytes = byteData!.buffer.asUint8List();
+
+    // Get the directory to save the file
+    final blob = html.Blob([combinedImageBytes]);
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    final anchor = html.AnchorElement(href: url)
+      ..setAttribute("download", "qr_code.png")
+      ..click();
+
+    html.Url.revokeObjectUrl(url);
+  } catch (err, stack) {
+    AppConfig.logger.d("err $err");
+    AppConfig.logger.d("stack $stack");
+  }
+}
+
+Future<void> saveFile(record) async {
+  try {
+    record = await record;
+    var qrData = {
+      "customer_id": record.id,
+    };
+
+    int qrImageSize = 300;
+    int padding = 360; // Gap between the QR code and the limits of the image
+    int textGap = 120; // Gap between the text and the QR code
+
+    final qrCode = QrCode.fromData(
+      data: json.encode(qrData),
+      errorCorrectLevel: QrErrorCorrectLevel.H,
+    );
+    final qrImage = QrImage(qrCode);
+    final qrImageBytes = await qrImage.toImageAsBytes(
+      size: qrImageSize,
+      format: ImageByteFormat.png,
+      decoration: const PrettyQrDecoration(),
+    );
+
+    // Convert ByteData to Uint8List
+    final buffer = qrImageBytes!.buffer;
+    final qrImageUint8List = buffer.asUint8List(qrImageBytes.offsetInBytes, qrImageBytes.lengthInBytes);
+
+    // Create an Image object from the QR code bytes
+    var codec = await instantiateImageCodec(qrImageUint8List);
+    FrameInfo frameInfo = await codec.getNextFrame();
+    var imgObj = frameInfo.image;
+
+    // Create an image with the text above the QR code
+    final recorder = PictureRecorder();
+    final canvas = Canvas(recorder);
+
+    // Define the text style
+    const textStyle = TextStyle(
+      color: Color(0xFF000000),
+      fontSize: 44,
+    );
+
+    // Create a text painter to measure and draw the text
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: "Aluno: ${record.name}",
+        style: textStyle,
+      ),
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout();
+
+    // Draw the text on the canvas
+    var horizontal = (qrImageSize + padding * 2 - textPainter.width) / 2;
+    final textOffset = Offset(
+      horizontal,
+      padding as double,
+    );
+
+    textPainter.paint(canvas, textOffset);
+
+    // Draw the QR code below the text with padding
+    final imageOffset = Offset(
+      padding as double,
+      padding + textPainter.height + textGap,
+    );
+
+    final paint = Paint();
+    canvas.drawImage(imgObj, imageOffset, paint);
+
+    // Calculate the final image size
+    int finalImageWidth = qrImageSize + padding * 2;
+    int finalImageHeight = qrImageSize + padding * 2 + textPainter.height.toInt() + textGap;
+
+    // Get the combined image as bytes
+    final combinedImage = await recorder.endRecording().toImage(finalImageWidth, finalImageHeight);
+    final byteData = await combinedImage.toByteData(format: ImageByteFormat.png);
+    final combinedImageBytes = byteData!.buffer.asUint8List();
+
+    // Get the directory to save the file
+    final blob = html.Blob([combinedImageBytes]);
+    final url = html.Url.createObjectUrlFromBlob(blob);
+    final anchor = html.AnchorElement(href: url)
+      ..setAttribute("download", "qr_code.png")
+      ..click();
+    html.Url.revokeObjectUrl(url);
+  } catch (err, stack) {
+    AppConfig.logger.d("err $err");
+    AppConfig.logger.d("stack $stack");
+  }
 }
